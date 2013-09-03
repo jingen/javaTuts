ade2495fcd933aa27cb922c8fcf9bb6d5ae05774
Jingen Lin <jingen.lin.jl@gmail.com>
2013-09-03 10:10:34 -0400

    201 complete : 2013-09-03 10:10:34 -0400 Tuesday

--- src/tuts/MyScanner.java
+++ src/tuts/MyScanner.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class MyScanner {
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
+
     System.out.println("Please input your full name:");
     String fullName = in.nextLine();
     System.out.println("Please input your age:");
